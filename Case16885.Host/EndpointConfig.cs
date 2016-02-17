c4362f38ee035ad853288c40e9f787f48e395f70
Bob Langley <Bob Langley>
2016-02-16 17:11:35 -0800

    Initial commit with Case versions

---
+++ Case16885.Host/EndpointConfig.cs
@@ -0,0 +1,13 @@
+
+namespace Case16885.Host
+{
+    using NServiceBus;
+
+    public class EndpointConfig : IConfigureThisEndpoint
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+
+        }
+    }
+}
